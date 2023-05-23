430bafa6a4d221f45259db0006e052a09f9a90d5
Bro74 <bmwhite1014@gmail.com>
2023-05-23 07:51:40 -0400

    debuged, created folder and basic logic for homeroutes and apiroutes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express =require("express")
-
+const homeRoutes= require("./routes/homeroutes")
+const apiRoutes= require("./routes/api-routes")
 
 const PORT = process.env.PORT || 3001;
 
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
+app.use(apiRoutes)
+app.use(homeRoutes)
 
  app.listen(PORT, () =>
     console.log(`Listening at http://localhost:${PORT}`)
